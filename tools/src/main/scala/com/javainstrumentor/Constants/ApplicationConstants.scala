c5cb85c9dc5629b6af753ae24b69da1101b34bca
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-12-08 15:53:08 -0600

    Constants for the module

---
+++ tools/src/main/scala/com/javainstrumentor/Constants/ApplicationConstants.scala
@@ -0,0 +1,11 @@
+package com.javainstrumentor.Constants
+
+object ApplicationConstants {
+
+  val JAVA_COMPILE_COMMAND = "javac "
+  val JAVA_EXECUTE_COMMAND = "java -cp "
+
+  val SERVER_IP = "127.0.0.1"
+  val SOCKET_PORT = 30
+
+}
