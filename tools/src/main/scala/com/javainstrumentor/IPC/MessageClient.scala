ca6107915cf8a2e316cae5ea7783213b3aec9eff
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-12-08 15:53:44 -0600

    IPC message client using sockets

---
+++ tools/src/main/scala/com/javainstrumentor/IPC/MessageClient.scala
@@ -0,0 +1,40 @@
+package com.javainstrumentor.IPC
+
+import java.io._
+import java.net._
+
+import com.javainstrumentor.Constants.ApplicationConstants
+import org.slf4j.{Logger, LoggerFactory}
+
+/**
+  * Opens a socket connection for message passing
+  */
+
+class MessageClient(ip: String, port: Int) {
+
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
+  val clientSocket = new Socket(ip, port)
+  val outputStreamWriter = new PrintWriter(clientSocket.getOutputStream, true)
+
+  def sendMessage(message: String): Unit = {
+    logger.info("Sending message {} to {}:{} ", message, ip, port)
+    outputStreamWriter.println(message)
+  }
+
+  def stopConnection(): Unit = {
+    outputStreamWriter.close()
+    clientSocket.close()
+  }
+
+}
+
+object MessageClient {
+  def main(args: Array[String]): Unit = {
+    val socketClient = new MessageClient(ApplicationConstants.SERVER_IP, ApplicationConstants.SOCKET_PORT)
+
+    val messages = List("Hi", "hello", "another message", "good bye", "!!")
+
+    messages.foreach(message => socketClient.sendMessage(message))
+
+  }
+}
