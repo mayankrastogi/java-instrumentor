9324b14857d83f7923bde6d81f301f6c1dfe3648
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-12-10 01:48:58 -0600

    added Instrumentor

---
+++ tools/src/main/scala/com/javainstrumentor/tool/parsing/Instrumentor.scala
@@ -0,0 +1,38 @@
+package com.javainstrumentor.tool.parsing
+
+import com.javainstrumentor.tool.Launcher.{project1OutputPath, project1Path}
+import com.javainstrumentor.tool.parsing.scopetable.ScopeTableItem
+
+class Instrumentor {
+
+  /**
+    * Instruments the given java project and returns the scope table for the project
+    *
+    * @param inputPath  The input path of the project to be instrumented
+    * @param outputPath The output path of the project to be instrumented
+    * @return ScopeTable
+    */
+
+  def instrumentAndFindScopeTable(inputPath: String, outputPath: String): Map[String, ScopeTableItem] = {
+
+    val javaProject = JavaProject(inputPath, outputPath, resolveFromResources = true)
+
+    var scopeTable: Map[String, ScopeTableItem] = Map.empty
+
+    javaProject.parsedSources.foreach { case (path, ast) =>
+      val visitor = new InstrumentationVisitor(ast, path)
+      ast.accept(visitor)
+      scopeTable ++= visitor.scopeTable
+      //    println(visitor.scopeTable)
+      println(ast)
+    }
+
+    println("ScopeTable: ")
+    scopeTable.values.foreach(println)
+
+
+    javaProject.writeInstrumentedProject()
+
+    scopeTable
+  }
+}
