bf362a2793b7e4edff66eaf263d5bd53a986cb13
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2019-12-10 17:10:17 -0600

    Add tests for JavaProject

---
+++ tools/src/test/resources/test-project/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+    public static void main(String[] args) {
+        SampleClass sampleClassObject;
+        sampleClassObject = new SampleClass();
+        int sampleMethodInput;
+        sampleMethodInput = 5;
+        int sampleMethodReturn;
+        sampleMethodReturn = sampleClassObject.sampleMethod(sampleMethodInput);
+    }
+}
