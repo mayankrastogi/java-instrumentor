a69d6f109bbcbccb7bf6f6c51804a6d90217863a
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2019-12-10 14:48:08 -0600

    Document Instrumentor client library.

--- client-lib/src/main/scala/com/javainstrumentor/clientlib/Instrumentor.scala
+++ client-lib/src/main/scala/com/javainstrumentor/clientlib/Instrumentor.scala
@@ -1,16 +1,39 @@
 package com.javainstrumentor.clientlib
 
-import scala.util.Try
-
+/**
+ * Client-library for sending instrumentation log messages to the execution server.
+ *
+ * The IP address, port, and the delimiter need to be specified using system properties while running the instrumented
+ * program.
+ *
+ * @see [[com.javainstrumentor.clientlib.ApplicationConstants#PROPERTY_SERVER_IP]],
+ *      [[com.javainstrumentor.clientlib.ApplicationConstants#PROPERTY_SERVER_PORT]],
+ *      [[com.javainstrumentor.clientlib.ApplicationConstants#PROPERTY_SERVER_DELIMITER]]
+ */
 object Instrumentor {
 
-  private lazy val client: MessageClient = new MessageClient(IPAddress, port)
+  /**
+   * The IP Address of the execution server.
+   */
   private lazy val IPAddress = System.getProperty(ApplicationConstants.PROPERTY_SERVER_IP)
+  /**
+   * The port of the execution server.
+   */
   private lazy val port = System.getProperty(ApplicationConstants.PROPERTY_SERVER_PORT).toInt
-  private lazy val delimiter = System.getProperty({
-    ApplicationConstants.PROPERTY_SERVER_DELIMITER
-  })
+  /**
+   * The delimiter to use for separating keys from values in a log message that is sent to the execution server.
+   */
+  private lazy val delimiter = System.getProperty(ApplicationConstants.PROPERTY_SERVER_DELIMITER)
 
+  //Initialize the client program when the first message is ready to be sent
+  private lazy val client: MessageClient = new MessageClient(IPAddress, port)
+
+  /**
+   * Sends the instrumentation log message to the execution server.
+   *
+   * @param key   The unique identifier of the variable whose value is being logged.
+   * @param value The current value of the variable.
+   */
   def log(key: String, value: Any): Unit = {
     val message = s"$key$delimiter${value.toString}"
 
