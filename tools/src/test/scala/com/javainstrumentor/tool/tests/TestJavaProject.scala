cb4cd4b319d02c7aa15a88dfb3084967c89efb22
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2019-12-10 19:09:45 -0600

    Finish test cases for JavaProject

--- tools/src/test/scala/com/javainstrumentor/tool/tests/TestJavaProject.scala
+++ tools/src/test/scala/com/javainstrumentor/tool/tests/TestJavaProject.scala
@@ -2,16 +2,21 @@ package com.javainstrumentor.tool.tests
 
 import com.javainstrumentor.clientlib.Instrumentor
 import com.javainstrumentor.tool.parsing.JavaProject
+import com.javainstrumentor.tool.parsing.scopetable.ScopeTableItem
 import com.javainstrumentor.tool.utils.IOUtils
-import org.scalatest.{Inspectors, Matchers, WordSpecLike}
+import org.scalatest._
 
-class TestJavaProject extends WordSpecLike with Matchers with Inspectors {
+class TestJavaProject extends WordSpecLike with Matchers with Inspectors with Inside with BeforeAndAfter {
 
   val testProjectDir = "test-project"
   val testProjectOutputDir = "."
   val testProjectDirFiles: List[String] = IOUtils.getJavaFilePathsFromResource(testProjectDir)
 
-  val project = JavaProject(testProjectDir, testProjectOutputDir, resolveFromResources = true)
+  var project: JavaProject = _
+
+  before {
+    project = JavaProject(testProjectDir, testProjectOutputDir, resolveFromResources = true)
+  }
 
   "A JavaProject, when parsed" must {
 
@@ -41,7 +46,7 @@ class TestJavaProject extends WordSpecLike with Matchers with Inspectors {
     }
 
     "add instrumentation log statements for all method declarations" in {
-      val sources = project.parsedSources
+      val sources = project.instrument.parsedSources
 
       val mainClassAST = sources.keys.filter(_.endsWith("MainClass.java")).map(sources(_)).head
       val sampleClassAST = sources.keys.filter(_.endsWith("SampleClass.java")).map(sources(_)).head
@@ -53,11 +58,136 @@ class TestJavaProject extends WordSpecLike with Matchers with Inspectors {
       sampleClassAST.toString.linesIterator.toIndexedSeq(3) should include regex sampleMethodLogRegex
     }
 
-    //    "add instrumentation log statements for all  variable declarations" in {
-    //      val sources = project.parsedSources
-    //
-    //      val mainClassAST = sources.keys.filter(_.endsWith("MainClass.java")).map(sources(_)).head
-    //      val sampleClassAST = sources.keys.filter(_.endsWith("SampleClass.java")).map(sources(_)).head
-    //    }
+    "add instrumentation log statements for all assignment expressions" in {
+      val sources = project.instrument.parsedSources
+
+      val mainClassAST = sources.keys.filter(_.endsWith("MainClass.java")).map(sources(_)).head.toString.linesIterator.toIndexedSeq
+      val sampleClassObjectRegex = """Instrumentor\.log\(".*(?<=MainClass~MainClass;\.main\(\[Ljava\/lang\/String;\)V#sampleClassObject",sampleClassObject\);)"""
+      val sampleMethodInputRegex = """Instrumentor\.log\(".*(?<=MainClass~MainClass;\.main\(\[Ljava\/lang\/String;\)V#sampleMethodInput",sampleMethodInput\);)"""
+      val sampleMethodReturnRegex = """Instrumentor\.log\(".*(?<=MainClass~MainClass;\.main\(\[Ljava\/lang\/String;\)V#sampleMethodReturn",sampleMethodReturn\);)"""
+
+      mainClassAST(6) should include regex sampleClassObjectRegex
+      mainClassAST(9) should include regex sampleMethodInputRegex
+      mainClassAST(12) should include regex sampleMethodReturnRegex
+
+      val sampleClassAST = sources.keys.filter(_.endsWith("SampleClass.java")).map(sources(_)).head.toString.linesIterator.toIndexedSeq
+      val numberRegex = """Instrumentor\.log\(".*(?<=SampleClass~SampleClass;\.sampleMethod\(I\)I#number",number\);)"""
+      val sampleMethodInputInSampleClassRegex = """Instrumentor\.log\(".*(?<=SampleClass~SampleClass;\.sampleMethod\(I\)I#sampleMethodInput#0#0",sampleMethodInput\);)"""
+
+      sampleClassAST(6) should include regex numberRegex
+      sampleClassAST(8) should include regex sampleMethodInputInSampleClassRegex
+    }
+
+    "add an entry in the scope table for all method declarations and their parameters" in {
+      val scopeTable = project.instrument.scopeTable.get.values
+
+      // MainClass
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("MainClass~MainClass;.main([Ljava/lang/String;)V")
+          name shouldBe "main"
+          dataType shouldBe "void"
+          methodArguments.get should contain only "String[] args"
+          parentClass shouldBe Some("MainClass")
+          filePath should endWith("MainClass.java")
+          lineNumber shouldBe 2
+        }
+      }
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("MainClass~MainClass;.main([Ljava/lang/String;)V#args#0#0")
+          name shouldBe "args"
+          dataType shouldBe "java.lang.String[]"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("MainClass.java")
+          lineNumber shouldBe 2
+        }
+      }
+
+      // SampleClass
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("SampleClass~SampleClass;.sampleMethod(I)I")
+          name shouldBe "sampleMethod"
+          dataType shouldBe "int"
+          methodArguments.get should contain only "int sampleMethodInput"
+          parentClass shouldBe Some("SampleClass")
+          filePath should endWith("SampleClass.java")
+          lineNumber shouldBe 2
+        }
+      }
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("SampleClass~SampleClass;.sampleMethod(I)I#sampleMethodInput#0#0")
+          name shouldBe "sampleMethodInput"
+          dataType shouldBe "int"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("SampleClass.java")
+          lineNumber shouldBe 2
+        }
+      }
+    }
+
+    "add an entry in the scope table for all variable declarations" in {
+      val scopeTable = project.instrument.scopeTable.get.values
+
+      // MainClass
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("MainClass~MainClass;.main([Ljava/lang/String;)V#sampleClassObject")
+          name shouldBe "sampleClassObject"
+          dataType shouldBe "SampleClass"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("MainClass.java")
+          lineNumber shouldBe 3
+        }
+      }
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("MainClass~MainClass;.main([Ljava/lang/String;)V#sampleMethodInput")
+          name shouldBe "sampleMethodInput"
+          dataType shouldBe "int"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("MainClass.java")
+          lineNumber shouldBe 5
+        }
+      }
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("MainClass~MainClass;.main([Ljava/lang/String;)V#sampleMethodReturn")
+          name shouldBe "sampleMethodReturn"
+          dataType shouldBe "int"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("MainClass.java")
+          lineNumber shouldBe 7
+        }
+      }
+
+      // SampleClass
+
+      forExactly(1, scopeTable) { item =>
+        inside(item) { case ScopeTableItem(id, name, dataType, methodArguments, parentClass, filePath, lineNumber) =>
+          id should endWith("SampleClass~SampleClass;.sampleMethod(I)I#number")
+          name shouldBe "number"
+          dataType shouldBe "int"
+          methodArguments shouldBe empty
+          parentClass shouldBe empty
+          filePath should endWith("SampleClass.java")
+          lineNumber shouldBe 3
+        }
+      }
+    }
   }
 }
